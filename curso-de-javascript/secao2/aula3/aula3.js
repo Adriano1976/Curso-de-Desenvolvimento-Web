3a0ca9be4a150e561bebf8b875a53325d52fb425
Adriano Santos <adriradiologista@hotmail.com.br>
2021-08-07 01:39:01 -0300

    Mudança de arquivo
    Os arquivos da pasta de JavaScript foi apagada do repositório Desenvolvimento web, pois a mesma foi clonada para outro repositório somente de JavaScript e TypeScript.

--- curso-de-javascript/secao2/aula3/aula3.js
+++
@@ -1,5 +0,0 @@
-console.log('Adriano Santos'); // String
-console.log("Adriano Santos"); // String
-console.log(`Adriano Santos`); // Strong
-console.log(123456); // Number
-console.log(35, 15.25, 'Adriano');
--- curso-de-javascript/secao2/aula5/index.js
+++
@@ -1,3 +0,0 @@
-// JavaScript em execução 
-console.log('Olá Mundo!');
-console.log('Esse trecho será exibido no console do navegador.');
