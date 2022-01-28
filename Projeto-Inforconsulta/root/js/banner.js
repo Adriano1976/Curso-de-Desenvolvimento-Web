1588c795453499978ff8cce472afe4d2ff84f52f
Adriano Santos <adriradiologista@hotmail.com.br>
2022-01-28 19:31:55 -0300

    Esse projeto mostra os conceitos de forma prática, mas simplificada.

---
+++ Projeto-Inforconsulta/root/js/banner.js
@@ -0,0 +1,10 @@
+var banners = ["img/banners/banner1.jpg","img/banners/banner2.jpg","img/banners/banner3.jpg",
+              "img/banners/banner4.jpg","img/banners/banner5.jpg","img/banners/banner6.jpg"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+  bannerAtual = (bannerAtual + 1) % 6;
+  document.querySelector('header img').src = banners[bannerAtual];
+}
+
+setInterval(trocaBanner, 4000);
